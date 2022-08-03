e16f46d018f32971c4fde1aca7b0e4df51e842ea
lexa21dff <jacordoba21@misena.edu.co>
2022-08-03 08:21:47 -0500

    segundo commit

--- ejercicio32.py
+++ ejercicio32.py
@@ -1,22 +1,25 @@
-numeros = [1,2,3,4,5,6,7,8,9,0]
+numeros = [1,2,3,4,5,6,7,8,9]
 t = 0
-
-
-while True:
+b = 0
+x = input('ingrese un valor ')
+numeros.append(x)
+for i in range(10):
     print(numeros)
-    x = int (input('ingrese un valor'))
-    # el valor ingresado debe ser mayor o igual que cero  y menor o menor o igual que 9 
-    # de lo contrario el ciclo se repite asta que ingrese un numero valido
-    if x >= 0  and x <= 9 :
-        for  i in range(x+1): 
-            # agrega los valores  al final del vector asta que sea igual a X
-            numeros.append(i)
-             # elimina los valores del inicio del vector asta que sea mayor que x 
-            numeros.remove(i)
-        
-        break
-    else:
-        print('el valor ingresado no es correcto')
-    
+    numeros=[]
+    t +=1
+    while True:
+        #derecha
+        b += 1
+        for j in range(1,10):
+            if j > t:
+                numeros.append(j)
+        if b == 1:
+            b = 0
+            break
+    # movimiento de x
+    numeros.append(x)
+    for l in range(1,t+1):
+        #izquierda
+        numeros.append(l)
 
-print(numeros)
+        
